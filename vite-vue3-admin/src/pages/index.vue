9e6d7c97c141a88465247e5d6e633afd493cb725
liuxia <1285370080@qq.com>
2022-05-10 13:44:14 +0800

    init

---
+++ vite-vue3-admin/src/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./../assets/logo.png" />
+  <use-pinia></use-pinia>
+  <br />
+  <router-link to="/login">点击跳转至login</router-link>
+  <br /><br />
+  <router-link to="/vueUse">点击跳转至vueUse页面</router-link>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import UsePinia from '@/components/UsePinia.vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {
+    UsePinia
+  }
+})
+</script>
