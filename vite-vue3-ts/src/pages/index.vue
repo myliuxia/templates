9cf2e18f2a6fd26321a9ba363c0d0cbd451eb5e1
liuxia <1285370080@qq.com>
2022-05-10 13:20:44 +0800

    update

--- vite-vue3-ts/src/pages/index.vue
+++ vite-vue3-ts/src/pages/index.vue
@@ -1,12 +1,10 @@
 <template>
   <img alt="Vue logo" src="./../assets/logo.png" />
-  <UsePinia></UsePinia>
+  <use-pinia></use-pinia>
   <br />
   <router-link to="/login">点击跳转至login</router-link>
   <br /><br />
   <router-link to="/vueUse">点击跳转至vueUse页面</router-link>
-  <br /><br />
-  <router-link to="/request">点击跳转至request请求页面</router-link>
 </template>
 
 <script lang="ts">
