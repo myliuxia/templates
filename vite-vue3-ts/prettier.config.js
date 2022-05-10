b16d90a14192d7d605e0df5ce87beba6deb05092
liuxia <1285370080@qq.com>
2022-05-10 10:42:20 +0800

    vite-vue3-ts init

---
+++ vite-vue3-ts/prettier.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false, //取消tab缩进
+  semi: false, // 句尾添加分号
+  vueIndentScriptAndStyle: true,
+  singleQuote: true, // 单引号
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  insertPragma: false,
+  requirePragma: false,
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'auto',
+  trailingComma: 'none' // 结尾逗号
+}
